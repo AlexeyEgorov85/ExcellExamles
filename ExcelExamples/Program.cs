ce5932a7ebe67790193ad3b0190995ec8ce5a9b9
Alexey Egorov <egorov_lexa2004@mail.ru>
2023-06-06 09:54:10 +0300

    refact: refactor main method

--- ExcelExamples/Program.cs
+++ ExcelExamples/Program.cs
@@ -1,53 +1,21 @@
 ﻿using ExcelExamples;
 using ExcelExamples.Domain;
+using Microsoft.Extensions.Configuration;
 using NLog;
 using System.Reflection;
 
 Logger logger = LogManager.GetCurrentClassLogger();
 
-args = new string[] {"OpenXML.dll"};
-
-if (!args.Any())
-{
-    logger.Fatal("Enter excel dll name");
-    return;
-}
-
-var dllPath = Path.Combine(AppContext.BaseDirectory, args[0]);
-if (!File.Exists(dllPath))
-{
-    logger.Fatal($"File '{dllPath}' does not exist");
-    return;
-}
-Assembly? dll;
 try
 {
-    dll = Assembly.LoadFrom(dllPath);
-}
-catch (Exception ex)
-{
-    logger.Fatal($"Failed to load dll '{dllPath}'.Error: {ex.Message}");
-    return;
-}
-
-var types = dll.GetExportedTypes()
-    .Where(p => typeof(IExcel).IsAssignableFrom(p) && !p.IsAbstract)
-    .ToArray();
-
-var excelExample = types.FirstOrDefault();
-if (excelExample == null)
-{
-    logger.Fatal($"Failed to find excel class in dll '{dllPath}'");
-    return;
-}
-
-try
-{
+    Config appConfig = GetConfig(logger);
+    Type excelExample = GetDllType(appConfig, logger);
     logger.Info($"------------Start {excelExample.Name}------------");
     var p = Activator.CreateInstance(excelExample);
     var p1 = p as IExcel;
     var data = FilmSeeder.Get();
     await p1!.RunAsync(data);
+    logger.Info("----------------End----------------");
 }
 catch (Exception ex)
 {
@@ -55,6 +23,59 @@ catch (Exception ex)
     logger.Debug(ex);
 }
 
-logger.Info("----------------End----------------");
 
+static Config GetConfig(ILogger logger)
+{
+    Config? appConfig = null;
 
+    try
+    {
+        appConfig = new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("config.json")
+        .Build()
+        .GetSection("Configuration")
+        .Get<Config>();
+    }
+    catch (Exception ex)
+    {
+        throw new Exception($"Failed to get config. Error: {ex.Message}");
+    }
+
+    return appConfig;
+}
+
+static Type GetDllType(Config? appConfig, ILogger logger)
+{
+    if (string.IsNullOrEmpty(appConfig?.DllName))
+    {
+        throw new ArgumentException("Enter excel dll name");
+    }
+
+    var dllPath = Path.Combine(AppContext.BaseDirectory, appConfig.DllName);
+    if (!File.Exists(dllPath))
+    {
+        throw new IOException($"File '{dllPath}' does not exist");
+    }
+    Assembly? dll;
+    try
+    {
+        dll = Assembly.LoadFrom(dllPath);
+    }
+    catch (Exception ex)
+    {
+        throw new IOException($"Failed to load dll '{dllPath}'.Error: {ex.Message}");
+    }
+
+    var types = dll.GetExportedTypes()
+        .Where(p => typeof(IExcel).IsAssignableFrom(p) && !p.IsAbstract)
+        .ToArray();
+
+    var excelExample = types.FirstOrDefault();
+    if (excelExample == null)
+    {
+        throw new IOException($"Failed to find excel class in dll '{dllPath}'");
+    }
+
+    return excelExample;
+}
